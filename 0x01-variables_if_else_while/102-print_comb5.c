b5025f398b9c39c01575c6391543991a7817eb9f
Ama-1 <Amamonney6960@gmail.com>
2023-02-24 05:19:03 -0800

    all task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -main entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (d = 0 ; d < 100; d++)
+		{
+			if (i < d)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((d / 10) + 48);
+				putchar((d % 10) + 48);
+				if (i != 98 || d != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
